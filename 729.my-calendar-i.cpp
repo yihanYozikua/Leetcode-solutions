f4000ba231dd476d6e3c156a5cc9fdbafb7a85c4
yihanYozikua <newy860318@gmail.com>
2022-02-24 12:07:20 +0800

    temp commit

--- 729.my-calendar-i.cpp
+++ 729.my-calendar-i.cpp
@@ -6,13 +6,17 @@
 
 // @lc code=start
 class MyCalendar {
+    vector<pair<int, int>> books;
 public:
     MyCalendar() {
-        
     }
     
     bool book(int start, int end) {
-        
+        for(pair<int, int> p : books){
+            if( max(p.first, start) < min(end, p.second) ) return false;
+        }
+        books.push_back({start, end});
+        return true;
     }
 };
 
